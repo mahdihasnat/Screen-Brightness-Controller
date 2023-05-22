3209d243ed33dbba1e93a72c67417bf3c711dd66
Mahdi Hasnat Siyam <44502454+mahdihasnat@users.noreply.github.com>
2023-05-23 01:22:53 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ Created on Thu Jan 14 17:19:54 2021
 """
 
 
-from tkinter import *
-
+from tkinter import Tk, Scale, HORIZONTAL, mainloop
 import screen_brightness_control as sbc
 
 def set_brightness(args):
@@ -15,15 +14,15 @@ def set_brightness(args):
         sbc.set_brightness(args)
     except sbc.ScreenBrightnessError as error:
         print(error)
-    
-    
+
+
 def slider():
     master = Tk()
     master.title('Screen_Brightness_Control')
     w = Scale(master, from_=0, to=100,tickinterval=5,length=600 ,
               orient=HORIZONTAL , command = set_brightness )
     w.set(sbc.get_brightness())
-    w.pack()    
+    w.pack()
     mainloop()
 
 slider()
